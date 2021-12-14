7cca8244d0b53adb88b6061b3dc8c306ab62b7c4
Jakob Ackermann <das7pad@outlook.com>
2021-12-14 22:53:46 +0100

    [web] add TODO for better handling of existing users on /register

--- services/web/pkg/managers/web/internal/userCreation/registerUser.go
+++ services/web/pkg/managers/web/internal/userCreation/registerUser.go
@@ -50,6 +50,7 @@ func (m *manager) RegisterUser(ctx context.Context, r *types.RegisterUserRequest
 	})
 	if err != nil {
 		if errors.GetCause(err) == user.ErrEmailAlreadyRegistered {
+			// TODO: consider letting the frontend try logging in instead.
 			err = m.lm.Login(ctx, &types.LoginRequest{
 				Session:   r.Session,
 				IPAddress: r.IPAddress,
