7d4c4394d0f10bee7c69155625b371500a33bb05
Jakob Ackermann <das7pad@outlook.com>
2023-01-08 23:58:19 +0100

    [web] learn: fix redirect from inside /learn/how-to namespace

--- services/web/pkg/managers/web/internal/learn/content.go
+++ services/web/pkg/managers/web/internal/learn/content.go
@@ -1,5 +1,5 @@
 // Golang port of Overleaf
-// Copyright (C) 2021 Jakob Ackermann <das7pad@outlook.com>
+// Copyright (C) 2021-2023 Jakob Ackermann <das7pad@outlook.com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
@@ -73,7 +73,12 @@ func (pc *pageContentRaw) parse(ps *templates.PublicSettings) *pageContent {
 		return &pageContent{exists: false}
 	}
 	if redirect := pc.redirect(); redirect != "" {
-		return &pageContent{redirect: redirect}
+		return &pageContent{
+			redirect: ps.SiteURL.
+				WithPath("/learn").
+				WithPath(redirect).
+				String(),
+		}
 	}
 	html := template.HTML(regexOverleafLinks.ReplaceAllString(
 		pc.Parse.Text.Star, ps.SiteURL.String()+"$1",
